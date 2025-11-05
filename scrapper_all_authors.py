49b2f33729c3a6c072b727bfc0cd245eef16eb09
Hushous <stueven.jan@web.de>
2025-11-05 17:28:15 +0100

    added author search, bug persisting at end of file

---
+++ scrapper_all_authors.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+titles_and_first_author = pd.read_csv("list_of_titles", sep=';', encoding='utf-8')
+
+API_KEY = os.getenv("KEY")
+
+titles_and_first_author = titles_and_first_author.drop(columns=["dc:creator"])
+titles_and_first_author = titles_and_first_author.rename(columns={"dc:identifier":"identifier","dc:title":"title","prism:coverDate":"coverDate","citedby-count":"citedCount"})
+
+for item in titles_and_first_author.head().index:
+    try:
+        SCOPUS_ID = titles_and_first_author["identifier"][item]
+
+        response = requests.get(f"https://api.elsevier.com/content/abstract/scopus_id/{SCOPUS_ID}", headers={"Accept": "application/json"}, params={"apiKey":API_KEY})
+        json_resp = response.json()
+
+        authors = ", ".join(
+            f"{a['preferred-name'].get('ce:given-name', '')} {a['preferred-name'].get('ce:surname', '')}".strip()
+            for a in json_resp.get('abstracts-retrieval-response', {}).get('authors', {}).get('author', [])
+        ) or None
+
+        titles_and_first_author.loc[item, "authors"] = authors
+        print(f"Adding {SCOPUS_ID} successful")
+
+    except Exception as err:
+        print(err)
+        continue
+        #BUG WITH AMBIGIOUS DATAFRAME maybe use len(df)
+
+if titles_and_first_author:
+    titles_and_first_author.to_csv("list_of_authors", sep=';', encoding='utf-8', index=False, header=True)
